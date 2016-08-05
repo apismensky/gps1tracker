4dee2f1743ad6351af922defdb03479b798cf7e8
eluzgin <eluzgin@gmail.com>
2016-08-05 17:49:06 -0600

    Added MongoDB library dep + init code

--- app/controllers/GpsController.scala
+++ app/controllers/GpsController.scala
@@ -2,10 +2,28 @@ package controllers
 
 import play.api.mvc._
 
+import org.mongodb.scala._
+import org.mongodb.scala.model.Aggregates._
+import org.mongodb.scala.model.Filters._
+import org.mongodb.scala.model.Projections._
+import org.mongodb.scala.model.Sorts._
+import org.mongodb.scala.model.Updates._
+import org.mongodb.scala.model._
+
 class GpsController extends Controller {
 
   def save = Action { request =>
+
+  	val mongoClient: MongoClient = MongoClient()
+
+  	val database: MongoDatabase = mongoClient.getDatabase("gps1db");
+
+  	val collection: MongoCollection[Document] = database.getCollection("gps1records")
+
+    mongoClient.close()
+
     println("Body: " + request.body)
+    
     Created("OK")
   }
 
--- build.sbt
+++ build.sbt
@@ -13,4 +13,6 @@ libraryDependencies ++= Seq(
   "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
 )
 
+libraryDependencies += "org.mongodb.scala" %% "mongo-scala-driver" % "1.0.1"
+
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
