2a3ca727c9032e7828a3eadb3797544ce6884f5f
eluzgin <eluzgin@gmail.com>
2016-08-05 18:10:46 -0600

    Add code to save record in MongoDB

--- app/controllers/GpsController.scala
+++ app/controllers/GpsController.scala
@@ -14,16 +14,30 @@ class GpsController extends Controller {
 
   def save = Action { request =>
 
+  	val record: String = request.body;
+
   	val mongoClient: MongoClient = MongoClient()
 
   	val database: MongoDatabase = mongoClient.getDatabase("gps1db");
 
   	val collection: MongoCollection[Document] = database.getCollection("gps1records")
 
-    mongoClient.close()
+  	val timestamp: Long = System.currentTimeMillis / 1000
+
+  	val doc: Document = Document("_id" -> timestamp, "record" -> record)
+
+  	val observable: Observable[Completed] = collection.insertOne(doc)
+
+      observable.subscribe(new Observer[Completed] {
+        override def onNext(result: Completed): Unit = println("Inserted")
+        override def onError(e: Throwable): Unit = println(" \n\nFailed " + e + "\n\n")
+        override def onComplete(): Unit = println("Completed")
+      })
+
+    mongoClient.close();
 
     println("Body: " + request.body)
-    
+
     Created("OK")
   }
 
